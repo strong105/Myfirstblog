e933b0dd5e7b396307aabb1ae4f580386f13d24c
study <n0isy321@gmail.com>
2019-06-19 15:08:41 +0300

    kek

---
+++ mysite/blog/models.py
@@ -0,0 +1,48 @@
+# from django.conf import settings
+# from django.db import models
+# from django.utils import timezone
+#
+#
+# class Post(models.Model):
+#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     text = models.TextField()
+#     created_date = models.DateTimeField(default=timezone.now)
+#     published_date = models.DateTimeField(blank=True, null=True)
+#
+#     def publish(self):
+#         self.published_date = timezone.now()
+#         self.save()
+#
+
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    def comm_count(self):
+        return Comments.objects.filter(answer_massage=self).count()
+
+    def comments(self):
+        return Comments.objects.filter(answer_massage=self)
+
+
+class Comments(models.Model):
+    date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text_massage = models.TextField()
+    answer_massage = models.ForeignKey(Post, on_delete=models.CASCADE)
+    answer_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.id)
---
+++ mysite/blog/views.py
@@ -0,0 +1,176 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.views import View
+from django.views.generic import CreateView, FormView
+
+from .models import Post, Comments
+from .forms import PostForm, CommentForm
+from django.template import Context, Template
+from django.http import HttpResponse
+from blog.models import *
+from django.views.generic.list import ListView
+
+from django.http import HttpResponseRedirect
+
+
+class PostListView(ListView, FormView):
+    model = Post
+    template_name = "post_list.html"
+    form_class = PostForm
+
+
+class CommentCreateView(View):
+    def post(self, request, post_pk, *args, **kwargs):
+         post = Post.objects.get(pk=post_pk)
+         Comments(
+             author=request.user, text_massage=request.POST['text'],
+             answer_massage=post
+         ).save()
+         return HttpResponseRedirect('/')
+
+
+def post_add(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            x = form.save(commit = False)
+            x.author = request.user
+            x.save()
+    return redirect("create_view")
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail_2', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit_2.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post_blog, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail_2', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit_2.html', {'form': form})
+
+
+
+#
+def test(request):
+    template = Template(open(r"D:/hlam/test.html").read())
+    context = Context({"my_name": "Adrian"})
+    template = template.render(context)
+    
+    return HttpResponse(template)
+    #return render(request, 'test.html', {})
+
+
+
+
+
+
+
+
+def test2(request):
+    
+
+    class R():
+        
+
+            
+        def __init__(self):
+            self.val=11
+            
+        """def __get__(self, obj, objtype):
+            return self.val"""
+
+        def __getitem__(self,index):
+            return "Ну допустим индекс " + str(index)
+
+        def __call__(self,x):
+            return "Вот мы и вызвали метод"
+
+    class Tipa_dict(dict):
+        test = R()
+        def __getattr__(self,name):
+                return getattr(dict,name)
+            
+    primer = Tipa_dict()
+
+    primer['test']=10
+    print(primer)
+    print(primer['test'])
+    print(primer.test)
+    print(primer.test(1))
+    print(primer.test[0])
+    
+
+    return render(request, 'blog/variable.html', {'primer': primer})
+
+
+def test_tags(request):
+
+    variables={'x':0,'y':[]}
+    variables['iter']= [(0,"Ноль"),(1,"one") ]
+    variables['dic'] = dict([(0,"Ноль"),(1,"one"),("f","one_")] )
+
+    return render(request, 'blog/tags.html', variables)
+
+
+
+
+def filters(request):
+
+    variables={'x':"привет"}
+    variables["many_words"]="The effect depends on the border-color value."
+    variables["smbl"]="e"
+    
+
+    return render(request, 'blog/filters.html', variables)
+
+def urli(request,*c,**d):
+    print(request.__dict__.keys())
+
+    return render(request, 'blog/urli.html', {'c':c,'d':d})
+
+
+def urli2(request,*c,**d):
+    # проверки
+    return redirect('urli', a='10')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
